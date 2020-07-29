b645df9a2caaf6bbc09d1b6e2dc0307dfb6a2748
popovei <popovei@stn-tmn-016.taxi.local>
2020-07-29 15:45:40 +0500

    added test

---
+++ src/test/java/com/example/crud/controller/AccountControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.crud.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountControllerTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
---
+++ src/test/java/com/example/crud/service/SkillServiceTest.java
@@ -0,0 +1,28 @@
+package com.example.crud.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SkillServiceTest {
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
